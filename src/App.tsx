3b4f5166ceb8f6c01651b8281fce2448eecb9ed2
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-06-22 15:56:01 -0300

    chore: add first components and add fontFamily in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import { gql, useQuery} from '@apollo/client'
-
-const GET_LESSON_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from './pages/Event'
 
 function App() {
-  const { data } = useQuery<{lessons : Lesson[]}>(GET_LESSON_QUERY)
-  console.log("🚀 ~ file: App.tsx ~ line 18 ~ App ~ data", data)
+
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+   <Event />
   )
 }
 
