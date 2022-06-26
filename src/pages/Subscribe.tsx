82a9882251f469e907b5fb400d6eae5596f1110c
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-06-26 12:03:30 -0300

    feat: add code generated types by schema graphql api

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,22 +1,16 @@
-import { gql, useMutation } from '@apollo/client';
 import { useState, FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Logo } from '../components/Logo';
+import { useCreateSubscribeMutation } from '../graphql/generated';
 
-const CREATE_SUBSCRIBE_MUTATION = gql`
-  mutation CreateSubscribe ($name:String!, $email: String!){
-  createSubscriber(data: {name: $name, email: $email}) {
-    id
-  }
-}
-`
+
 export function Subscribe() {
   const navigate = useNavigate()
 
   const [ name, setName ] = useState("")
   const [ email, setEmail ] = useState("")
 
-  const [createSubscribe , { loading }] = useMutation(CREATE_SUBSCRIBE_MUTATION)
+  const [createSubscribe , { loading }] = useCreateSubscribeMutation()
 
   async function handleSubscribe(event: FormEvent ) {
     event.preventDefault()
